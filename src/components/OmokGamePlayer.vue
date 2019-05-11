24301131c9cee0475da41e1e9de897b83a0db9e5
Min Gyu Park <parkmg@myumanitoba.ca>
2019-05-10 19:54:05 -0500

    Add player component

---
+++ src/components/OmokGamePlayer.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="omok-game-player">
+    <input v-model="playerName" type="text" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "OmokGamePlayer",
+  props: {
+    num: {
+      type: Number,
+      required: true
+    }
+  },
+  computed: {
+    playerName: {
+      get() {
+        return this.$store.getters["player/playerName"](this.num);
+      },
+      set(newName) {
+        this.$store.dispatch("player/updatePlayerName", {
+          num: this.num,
+          name: newName
+        });
+      }
+    }
+  }
+};
+</script>
+
+<style></style>
--- src/store/modules/player.js
+++ src/store/modules/player.js
@@ -7,16 +7,24 @@ export default {
   getters: {
     curr: state => state.curr,
     currInfo: state => state.players[state.curr],
-    pieceUrl: (state, getters, rootState, rootGetters) => i => {
+    playerName: state => num => state.players[num].name,
+    playerPiece: state => num => state.players[num].piece,
+    pieceUrl: (state, getters, rootState, rootGetters) => num => {
       const theme = rootGetters["theme/curr"];
-      const playerPiece = state.players[i].piece;
+      const playerPiece = state.players[num].piece;
       return require(`@/assets/img/${theme.name}/${theme.pieces[playerPiece]}`);
     }
   },
   mutations: {
     // TO-DO
-    updatePiece: (state, payload) =>
-      (state.players[payload.player].piece = payload.piece),
+    updateName: (state, { num, name }) => (state.players[num].name = name),
+    updatePiece: (state, { num, piece }) => (state.players[num].piece = piece),
     toggleCurr: state => (state.curr = (state.curr + 1) % 2) // 0 or 1
+  },
+  actions: {
+    updatePlayerName: ({ commit }, payload) => {
+      commit("updateName", payload);
+    },
+    updatePlayerPiece: ({ commit }, payload) => commit("updatePiece", payload)
   }
 };
